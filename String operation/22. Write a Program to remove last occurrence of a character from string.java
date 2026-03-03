f7850816d7bfc1f5bfd8284a70f58ce5c917471b
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:26:51 +0530

    Update 22. Write a Program to remove last occurrence of a character from string.java

--- String operation/22. Write a Program to remove last occurrence of a character from string.java
+++ String operation/22. Write a Program to remove last occurrence of a character from string.java
@@ -1,1 +1,11 @@
-
+	public String remove_last(String str, char ch) {
+		int index = 0;
+		for(int i= 0;i<str.length();i++) {
+			if(str.charAt(i) == ch) {
+				index = i;
+			}
+		}
+		String result = str.substring(0,index)+str.substring(index+1, str.length());
+		System.out.println(result);
+		return result;
+	}
