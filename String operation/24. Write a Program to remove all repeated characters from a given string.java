89482b151293a619a439fdead47cb315d5203e55
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:25:31 +0530

    Add method to remove duplicate characters from string
    Implemented a method to remove duplicate characters from a string.

--- String operation/24. Write a Program to remove all repeated characters from a given string.java
+++ String operation/24. Write a Program to remove all repeated characters from a given string.java
@@ -1,1 +1,14 @@
-
+	public void remove_dup_char(String str) {
+		Set<Character> seen = new LinkedHashSet<>();
+		for(char c: str.toCharArray()) {
+			seen.add(c);
+		}
+		
+		StringBuilder result = new StringBuilder();
+		for(char c: seen) {
+			result.append(c);
+		}
+		System.out.println(result.toString());
+		
+		
+	}
