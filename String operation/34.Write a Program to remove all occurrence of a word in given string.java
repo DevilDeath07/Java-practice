2f1bf15b1a4ffa8301f73e52499e8a8b89a21d0f
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:18:42 +0530

    Add method to remove all occurrences of a word
    Implement method to remove all occurrences of a specified word from a given string and print the modified string.

--- String operation/34.Write a Program to remove all occurrence of a word in given string.java
+++ String operation/34.Write a Program to remove all occurrence of a word in given string.java
@@ -1,1 +1,9 @@
-
+public void remove_all_occur(String str, String key) {
+  int count = str.indexOf(key);
+  
+      while(count != -1) {
+        str = str.substring(0, count)+str.substring(count+key.length()+1, str.length());
+      System.out.println(str);
+        count = str.indexOf(key, count+1);
+      }
+}
