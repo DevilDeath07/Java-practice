b197fc852290621deb2d5a5a935c699dd15370c5
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:27:36 +0530

    Add method to remove first occurrence of a character
    Implement method to remove the first occurrence of a character from a string.

--- String operation/21. Write a Program to remove first occurrence of a character from string.java
+++ String operation/21. Write a Program to remove first occurrence of a character from string.java
@@ -1,1 +1,8 @@
-
+	public void remove_first_occur(String str,char ch) {
+		for(int i =0;i<=str.length();i++) {
+			if (str.charAt(i) == ch) {
+				System.out.println(str.substring(i+1, str.length()));
+				break;
+			}
+		}
+	}
