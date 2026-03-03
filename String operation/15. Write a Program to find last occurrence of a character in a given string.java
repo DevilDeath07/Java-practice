2792131c7e91ed1d0afe7496624cd47714a18d67
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:35:30 +0530

    Add last_occurance method to find character index
    Implement method to find last occurrence of a character in a string.

--- String operation/15. Write a Program to find last occurrence of a character in a given string.java
+++ String operation/15. Write a Program to find last occurrence of a character in a given string.java
@@ -1,1 +1,9 @@
-
+	public int last_occurance(String str,char key) {
+		int index = 0;
+		for(int i= 0;i<str.length();i++) {
+			if(str.charAt(i) == key) {
+				index = i;
+			}
+		}
+		return index;
+	}
