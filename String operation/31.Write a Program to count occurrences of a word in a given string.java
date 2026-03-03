6992e3727defa8977eeee1dbea8fc41a87785be4
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:19:59 +0530

    Implement word occurrence counting method
    Added a method to count occurrences of a word in a string.

--- String operation/31.Write a Program to count occurrences of a word in a given string.java
+++ String operation/31.Write a Program to count occurrences of a word in a given string.java
@@ -1,1 +1,9 @@
-
+public void count_occur(String str, String key) {
+  int count = str.indexOf(key);
+  int result = 0;
+      while(count != -1) {
+        result++;
+        count = str.indexOf(key, count+1);
+      }
+      System.out.println(key+" occured in "+ result);
+}
