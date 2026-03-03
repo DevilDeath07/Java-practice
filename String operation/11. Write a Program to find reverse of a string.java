6ab9243f51d702a78f2d0b425ecc3437a29128aa
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:31:27 +0530

    Implement string reversal method
    Added a method to reverse a given string and print the result.

--- String operation/11. Write a Program to find reverse of a string.java
+++ String operation/11. Write a Program to find reverse of a string.java
@@ -1,1 +1,8 @@
-
+	public String reverse_string(String str) {
+		String result = "";
+		for(int i=str.length()-1;i>=0;i--) {
+			result = result+str.charAt(i);
+		}
+		System.out.println(result);
+		return result;
+	}
