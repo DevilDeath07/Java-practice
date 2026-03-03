cfbc4138ff6d94c54b98054c39304768d4120b0f
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:36:13 +0530

    Add method to count character occurrences in string

--- String operation/17. Write a Program to count occurrences of a character in given string.java
+++ String operation/17. Write a Program to count occurrences of a character in given string.java
@@ -1,1 +1,9 @@
-
+	public int occurance_count(String str, char key) {
+		int count = 0;
+		for(int i= 0;i<str.length();i++) {
+			if(str.charAt(i) == key) {
+				count++;
+			}
+		}
+		return count;
+	}
