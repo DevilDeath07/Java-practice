9d2df762d098fb4e0bc46bc165cc00de4b159ec5
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:24:37 +0530

    Add method to swap first character occurrence
    Implement method to replace first occurrence of a character in a string.

--- String operation/25. Write a Program to replace first occurrence of a character with another in a string.java
+++ String operation/25. Write a Program to replace first occurrence of a character with another in a string.java
@@ -1,1 +1,16 @@
+	public void swap_first(String str, char ch,char swap) {
+		int index = 0;
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==ch) {
+				index = i;
+				break;
+			}
+		}
+		// Convert to char array for mutability
+        char[] chars = str.toCharArray();
 
+        
+        chars[index] = swap;
+        String result = new String(chars);
+        System.out.println(result);
+	}
