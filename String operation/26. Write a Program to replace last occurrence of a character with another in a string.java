00a8e5fca2848fea4559365d6665c1fc37e13527
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:24:06 +0530

    Add method to swap last character occurrence
    Implemented a method to replace the last occurrence of a character in a string.

--- String operation/26. Write a Program to replace last occurrence of a character with another in a string.java
+++ String operation/26. Write a Program to replace last occurrence of a character with another in a string.java
@@ -1,1 +1,15 @@
+public void Swap_last(String str, char ch, char swap) {
+  int index = 0;
+  for(int i= 0;i<str.length();i++) {
+    if(str.charAt(i) == ch) {
+      index = i;
+    }
+  }
+  // Convert to char array for mutability
+      char[] chars = str.toCharArray();
 
+      
+      chars[index] = swap;
+      String result = new String(chars);
+      System.out.println(result);
+}
