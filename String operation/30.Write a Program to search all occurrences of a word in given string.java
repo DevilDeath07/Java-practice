b0f7b86545aa5cf40fbb18ce592e117428271de1
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:20:23 +0530

    Implement method to search all occurrences of a word
    Added a method to find and print all occurrences of a specified word in a given string.

--- String operation/30.Write a Program to search all occurrences of a word in given string.java
+++ String operation/30.Write a Program to search all occurrences of a word in given string.java
@@ -1,1 +1,9 @@
 
+public void all_occur(String str, String key) {
+  int count = str.indexOf(key);
+  
+      while(count != -1) {
+        System.out.println(key+" Starts at "+count);
+        count = str.indexOf(key, count+1);
+      }
+}
