1a98654213a31169ed1e7345a546e4cb187569f3
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:19:35 +0530

    Add method to remove first occurrence of a word
    Implemented method to remove the first occurrence of a specified word from a string.

--- String operation/32.Write a Program to remove first occurrence of a word from string.java
+++ String operation/32.Write a Program to remove first occurrence of a word from string.java
@@ -1,1 +1,5 @@
-
+public void remove_first_occur(String str, String key) {
+  int count = str.indexOf(key);
+  String result = str.substring(0, count)+str.substring(count+key.length()+1, str.length());
+  System.out.println(result);
+}
