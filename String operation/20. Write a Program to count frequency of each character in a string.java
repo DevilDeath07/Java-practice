d02e8619206e644c8231bd9ba1f05ef648241a14
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:28:43 +0530

    Add character frequency counting methods

--- String operation/20. Write a Program to count frequency of each character in a string.java
+++ String operation/20. Write a Program to count frequency of each character in a string.java
@@ -1,1 +1,22 @@
-
+    public int occurance_count(String str, char key) {
+        int count = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == key) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void count_frequency(String str) {
+        HashSet<Character> visited = new HashSet<>();
+        frequency_char fc1 = new frequency_char();
+        
+        for(char ch : str.toCharArray()) {
+            if(!visited.contains(ch)) {  // only count if not already visited
+                int temp = fc1.occurance_count(str, ch);
+                visited.add(ch);
+                System.out.println(ch + " = " + temp);
+            }
+        }
+    }
