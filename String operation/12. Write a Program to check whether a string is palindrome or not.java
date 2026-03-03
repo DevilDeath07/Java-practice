ea9a1a7aeee152c90df2749acaf43c32e484eec3
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:32:11 +0530

    Add palindrome check program in Java

--- String operation/12. Write a Program to check whether a string is palindrome or not.java
+++ String operation/12. Write a Program to check whether a string is palindrome or not.java
@@ -1,1 +1,23 @@
+public class Check_palindrome {
+	public boolean palindrome(String str) {
+		String result = "";
+		for(int i=str.length()-1;i>=0;i--) {
+			result = result+str.charAt(i);
+		}		
+		return str.equals(result);
+	}
+
+	public static void main(String[] args) {
+		String str = "madam";
+		Check_palindrome cp = new Check_palindrome();
+		if(cp.palindrome(str)) {
+			System.out.println("palindrome");
+		}
+		else {
+			System.out.println("not palindrome");
+		}
+
+	}
+
+}
 
