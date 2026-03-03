b75d64b6bcba8f76ab7c1d29afd90f24bccbd590
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 21:19:10 +0530

    Add method to remove last occurrence of a word
    Implement method to remove the last occurrence of a specified word from a string.

--- String operation/33.Write a Program to remove last occurrence of a word in given string.java
+++ String operation/33.Write a Program to remove last occurrence of a word in given string.java
@@ -1,1 +1,6 @@
 
+public void remove_last_occur(String str, String key) {
+  int count = str.lastIndexOf(key);
+  String result = str.substring(0, count)+str.substring(count+key.length()+1, str.length());
+  System.out.println(result);
+}
