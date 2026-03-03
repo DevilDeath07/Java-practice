df08ab30d9a3401ddf309047fcf27fc387c36980
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 22:10:48 +0530

    Implement program to find lowest frequency character

--- String operation/19. Write a Program to find lowest frequency character in a string.java
+++ String operation/19. Write a Program to find lowest frequency character in a string.java
@@ -1,1 +1,24 @@
+public class LowestFrequencyChar {
+    public static void main(String[] args) {
+        String str = "I love learning programming at Codeforwin.";
 
+        // Count frequency of each character
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+
+        // Find character with minimum occurrence
+        char minChar = '\0';
+        int minCount = Integer.MAX_VALUE;
+
+        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() < minCount) {
+                minCount = entry.getValue();
+                minChar = entry.getKey();
+            }
+        }
+
+        System.out.println("Minimum occurring character is '" + minChar + "' = " + minCount);
+    }
+}
