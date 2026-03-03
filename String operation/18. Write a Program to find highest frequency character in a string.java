17334cc7a11c71884d18b14360c3526fefa50732
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 22:17:08 +0530

    Add program to find highest frequency character in string

--- String operation/18. Write a Program to find highest frequency character in a string.java
+++ String operation/18. Write a Program to find highest frequency character in a string.java
@@ -1,1 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
 
+public class HigherFrequencyChar {
+	public static void main(String[] args) {
+        String str = "I love Codeforwin.";
+
+        // Count frequency of each character
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char ch : str.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+
+        // Find character with minimum occurrence
+        char maxChar = '\0';
+        int maxCount = Integer.MIN_VALUE;
+
+        for (char ch : str.toCharArray()) { 
+        	int count = freq.get(ch); 
+        	if (count > maxCount) { 
+        		maxCount = count; 
+        		maxChar = ch; 
+        		} 
+        	}
+
+        System.out.println("Minimum occurring character is '" + maxChar + "' = " + maxCount);
+    }
+}
