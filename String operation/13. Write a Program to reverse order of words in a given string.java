950bd8eb8fd8dd6f4dddffa1452c0a1af66a98fc
Balamurugan V <balamurugankumar880@gmail.com>
2026-03-03 22:25:48 +0530

    Add method to reverse order of words in string
    Implemented a method to reverse the order of words in a given string.

--- String operation/13. Write a Program to reverse order of words in a given string.java
+++ String operation/13. Write a Program to reverse order of words in a given string.java
@@ -1,1 +1,13 @@
-
+public void reverse_order_word(String str) {
+  String result = "";
+  int prev = str.length();
+  for(int i = str.length()-1;i>=0;i--) {
+    if(str.charAt(i)==' ') {
+      CharSequence sub = str.subSequence(i, prev);
+      prev = i;
+      result = result + sub.toString();
+      
+    }
+  }
+  System.out.println(result);
+}
